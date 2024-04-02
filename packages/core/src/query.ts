74260b13b1a0b56749e3a8bcfee5f564ef451034
Leon Coto <leoncoto@gmail.com>
2024-04-02 20:49:20 +0100

    run queries over embeddings

--- packages/core/src/query.ts
+++ packages/core/src/query.ts
@@ -1,6 +1,19 @@
 import { createEmbedding } from "./lib/createEmbedding";
+import { Pool } from "pg";
+const db = new Pool();
 
 export const queryCommand = async (query: string) => {
   const response = await createEmbedding(query);
   const embedding = response.data[0]?.embedding;
+  if (!embedding) {
+    console.log(`Embedding creation failed for ${query}`);
+    return;
+  }
+
+  const result = await db.query({
+    text: `SELECT * FROM pages ORDER BY embedding <=> $1 LIMIT 1`,
+    values: [JSON.stringify(embedding)],
+  });
+
+  console.log(result.rows.map((x) => x.document));
 };
